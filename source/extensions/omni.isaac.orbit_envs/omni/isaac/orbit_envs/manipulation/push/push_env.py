3541ba1189cc98c9d025b7fa5bc8d359eb230fda
junjungoal <junyamada107@gmail.com>
2023-03-24 14:09:37 +0000

    modify path

--- source/extensions/omni.isaac.orbit_envs/omni/isaac/orbit_envs/manipulation/push/push_env.py
+++ source/extensions/omni.isaac.orbit_envs/omni/isaac/orbit_envs/manipulation/push/push_env.py
@@ -83,7 +83,7 @@ class PushEnv(IsaacEnv):
         prim_utils.create_prim(self.template_env_ns + "/Table", usd_path=self.cfg.table.usd_path)
 
         add_reference_to_stage(os.path.join(os.environ['ORBIT_PATH'],
-                                            'source/extensions/omni.isaac.orbit_envs/omni/isaac/orbit_envs/manipulation/push/assets/goal_marker.usda'),
+                                            'source/extensions/omni.isaac.orbit_envs/omni/isaac/orbit_envs/manipulation/push/assets/goal_marker_instanceable.usd'),
                                self.template_env_ns + '/GoalMarker')
 
         # robot
