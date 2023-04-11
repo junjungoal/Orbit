ec96583b611a24840c78c2e44f3b4ca0e2941b6c
junjungoal <junyamada107@gmail.com>
2023-04-11 00:43:29 +0000

    modify threshold

--- source/extensions/omni.isaac.orbit_envs/omni/isaac/orbit_envs/manipulation/push/push_cfg.py
+++ source/extensions/omni.isaac.orbit_envs/omni/isaac/orbit_envs/manipulation/push/push_cfg.py
@@ -170,9 +170,9 @@ class RewardsCfg:
     # penalizing_arm_action_rate_l2 = {"weight": 1e-2}
     # penalizing_tool_action_l2 = {"weight": 1e-2}
     # -- object-centric
-    # tracking_object_position_exp = {"weight": 5.0, "sigma": 0.25, "threshold": 0.08}
     tracking_object_position_tanh = {"weight": 5.0, "sigma": 0.2, "threshold": 0.12}
-    push_object_success = {"weight": 3.5, "threshold": 0.005}
+    # tracking_object_position_tanh = {"weight": 5.0, "sigma": 0.2, "threshold": 0.11}
+    push_object_success = {"weight": 3.5, "threshold": 0.01}
 
 
 @configclass
