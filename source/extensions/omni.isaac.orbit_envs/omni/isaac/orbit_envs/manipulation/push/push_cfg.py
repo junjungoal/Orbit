6a87b5fc95c73e883e4c2c5c2bcddfe31fae00d7
junjungoal <junyamada107@gmail.com>
2023-04-15 10:14:27 +0000

    modify reward ratio

--- source/extensions/omni.isaac.orbit_envs/omni/isaac/orbit_envs/manipulation/push/push_cfg.py
+++ source/extensions/omni.isaac.orbit_envs/omni/isaac/orbit_envs/manipulation/push/push_cfg.py
@@ -167,7 +167,7 @@ class RewardsCfg:
     # tracking_object_position_exp = {"weight": 5.0, "sigma": 0.2, "threshold": 0.1}
     # push_object_success = {"weight": 3.5, "threshold": 0.04}
     reaching_object_position_negative = {"weight": 1,}
-    tracking_object_position_negative = {"weight": 5,}
+    tracking_object_position_negative = {"weight": 3,}
     push_object_success = {"weight": 1, "threshold": 0.04}
 
 
