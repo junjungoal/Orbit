e2c1d16dc58545ce531668488b17af7b14ab2b89
junjungoal <junyamada107@gmail.com>
2023-04-15 01:50:10 +0000

    modify observation

--- source/extensions/omni.isaac.orbit_envs/omni/isaac/orbit_envs/manipulation/push/push_cfg.py
+++ source/extensions/omni.isaac.orbit_envs/omni/isaac/orbit_envs/manipulation/push/push_cfg.py
@@ -138,8 +138,8 @@ class ObservationsCfg:
         tool_positions = {"scale": 1.0}
         # tool_orientations = {"scale": 1.0}
         # -- object state
-        # object_positions = {"scale": 1.0}
-        # object_orientations = {"scale": 1.0}
+        object_positions = {"scale": 1.0}
+        object_orientations = {"scale": 1.0}
         object_relative_tool_positions = {"scale": 1.0}
         # object_relative_tool_orientations = {"scale": 1.0}
         # -- object desired state
