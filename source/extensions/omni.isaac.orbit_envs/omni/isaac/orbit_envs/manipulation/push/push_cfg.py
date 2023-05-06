d6e9af86eda8cb54334748191aca694d9579d6b8
junjungoal <junyamada107@gmail.com>
2023-05-06 13:04:25 +0000

    randomize more

--- source/extensions/omni.isaac.orbit_envs/omni/isaac/orbit_envs/manipulation/push/push_cfg.py
+++ source/extensions/omni.isaac.orbit_envs/omni/isaac/orbit_envs/manipulation/push/push_cfg.py
@@ -107,8 +107,8 @@ class RandomizationCfg:
         orientation_cat: str = "default"  # randomize position: "default", "uniform"
         # randomize position
         position_default = [0.45, 0.0, 0.045]  # position default (x,y,z)
-        position_uniform_min = [0.45, -0.05, 0.045]  # position (x,y,z)
-        position_uniform_max = [0.5, 0.05, 0.045]  # position (x,y,z)
+        position_uniform_min = [0.45, -0.15, 0.045]  # position (x,y,z)
+        position_uniform_max = [0.5, 0.15, 0.045]  # position (x,y,z)
 
     @configclass
     class GoalPoseCfg:
@@ -118,8 +118,8 @@ class RandomizationCfg:
         position_cat: str = "uniform"  # randomize position: "default", "uniform"
         # randomize position
         position_default = [0.6, 0., 0.0]  # position default (x,y,z)
-        position_uniform_min = [0.6, -0.02, 0.]  # position (x,y,z)
-        position_uniform_max = [0.6, 0.02, 0.]  # position (x,y,z)
+        position_uniform_min = [0.6, -0.1, 0.]  # position (x,y,z)
+        position_uniform_max = [0.65, 0.1, 0.]  # position (x,y,z)
 
     # initialize
     object_initial_pose: ObjectInitialPoseCfg = ObjectInitialPoseCfg()
