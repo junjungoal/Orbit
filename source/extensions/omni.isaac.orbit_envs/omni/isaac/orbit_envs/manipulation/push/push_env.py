08b4b886dd424cc0e81efe8e4466a02e3800add8
junjungoal <junyamada107@gmail.com>
2023-06-23 08:05:08 +0000

    remove far

--- source/extensions/omni.isaac.orbit_envs/omni/isaac/orbit_envs/manipulation/push/push_env.py
+++ source/extensions/omni.isaac.orbit_envs/omni/isaac/orbit_envs/manipulation/push/push_env.py
@@ -567,14 +567,14 @@ class PushRewardManager(RewardManager):
         num_tool_sites = tool_sites_distance.shape[1]
         average_distance = (ee_distance + torch.sum(tool_sites_distance, dim=1)) / (num_tool_sites + 1)
 
-        success = torch.where(torch.norm(env.object.data.root_pos_w[:, :2]-env.goal.data.root_pos_w[:, :2], dim=1) < threshold, True, False)
-        ee_to_obj = torch.norm(env.object.data.root_pos_w-env.robot.data.ee_state_w[:, 0:3], dim=1)
-        not_too_far_away = ee_to_obj < 0.1
-        idx = torch.logical_and(success, ee_to_obj)
-        reward = 1 - torch.tanh(ee_distance / sigma)
-        reward[idx] = 1.
-        return reward
-        # return 1 - torch.tanh(ee_distance / sigma)
+        # success = torch.where(torch.norm(env.object.data.root_pos_w[:, :2]-env.goal.data.root_pos_w[:, :2], dim=1) < threshold, True, False)
+        # ee_to_obj = torch.norm(env.object.data.root_pos_w-env.robot.data.ee_state_w[:, 0:3], dim=1)
+        # not_too_far_away = ee_to_obj < 0.1
+        # idx = torch.logical_and(success, ee_to_obj)
+        # reward = 1 - torch.tanh(ee_distance / sigma)
+        # reward[idx] = 1.
+        # return reward
+        return 1 - torch.tanh(ee_distance / sigma)
 
     def penalizing_action_rate_l2(self, env: PushEnv):
         """Penalize large variations in action commands."""
