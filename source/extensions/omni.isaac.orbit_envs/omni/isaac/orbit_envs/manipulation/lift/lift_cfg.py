10050161e3b3892b79f99c52e486af9c77597086
junjungoal <junyamada107@gmail.com>
2023-06-29 17:58:29 +0000

    modify friction

--- source/extensions/omni.isaac.orbit_envs/omni/isaac/orbit_envs/manipulation/lift/lift_cfg.py
+++ source/extensions/omni.isaac.orbit_envs/omni/isaac/orbit_envs/manipulation/lift/lift_cfg.py
@@ -55,7 +55,7 @@ class ManipulationObjectCfg(RigidObjectCfg):
         disable_gravity=False,
     )
     physics_material = RigidObjectCfg.PhysicsMaterialCfg(
-        static_friction=1.5, dynamic_friction=1.5, restitution=0., prim_path="/World/Materials/cubeMaterial"
+        static_friction=1.6, dynamic_friction=1.6, restitution=0.1, prim_path="/World/Materials/cubeMaterial"
     )
 
 
