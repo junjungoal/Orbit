a9e2b6928266922301867c5ee5d2f0c19eb14852
junjungoal <junyamada107@gmail.com>
2023-07-22 17:39:38 +0000

    modify cfg

--- source/extensions/omni.isaac.orbit_envs/omni/isaac/orbit_envs/manipulation/lift/lift_cfg.py
+++ source/extensions/omni.isaac.orbit_envs/omni/isaac/orbit_envs/manipulation/lift/lift_cfg.py
@@ -172,10 +172,11 @@ class RewardsCfg:
     reaching_object_position_tanh = {"weight": 1., "sigma": 10}
     opening_gripper = {'weight': 0.1}
     # tracking_object_position_tanh = {"weight": 5., "sigma": 0.2}
-    tracking_object_position_tanh = {"weight": 2., "sigma": 5}
+    tracking_object_position_tanh = {"weight": 1., "sigma": 5}
     # penalizing_action_rate_l2 = {"weight": 0.05}
     grasp_object_success = {'weight': 0.25}
-    lifting_object_success = {"weight": 3.25, "threshold": 0.08}
+    # lifting_object_success = {"weight": 3.25, "threshold": 0.08}
+    lifting_object_success = {"weight": 1, "threshold": 0.08}
 
 
 @configclass
